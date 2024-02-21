9018a592e51bc4fe7c54219b8379bceeff09308e
sonyanchas <115482749+sonyanchas@users.noreply.github.com>
2024-02-21 13:56:17 -0500

    Audio

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -6,7 +6,10 @@ public class Controller : MonoBehaviour
 {
     [SerializeField] float MinY;
     [SerializeField] float MaxY;
+    [SerializeField] float MinX;
+    [SerializeField] float MaxX;
     [SerializeField] float Speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +24,30 @@ public class Controller : MonoBehaviour
 
         transform.Translate(Time.deltaTime * Speed * y * Vector3.up);
         transform.Translate(Time.deltaTime * Speed * x * Vector3.right);
-
-        //if transform.position.y > MaxY )
-       // {
-            //transform.position
-        //}
+        if (transform.position.y > MaxY)
+        {
+            transform.position = new Vector3(transform.position.x, MaxY, 0f);
+        }
+        if (transform.position.y < MinY)
+        {
+            transform.position = new Vector3(transform.position.x, MinY, 0f);
+        }
+        if (transform.position.x > MaxX)
+        {
+            transform.position = new Vector3(MaxX, transform.position.y, 0f);
+        }
+        if (transform.position.x < MinX)
+        {
+            transform.position = new Vector3(MinX, transform.position.y, 0f);
+        }
+        if (Input.GetButton("Fire1") == true)
+            {
+            AudioSource audioSource;
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+        }
     }
-}
+    }
+    
+    
                          
